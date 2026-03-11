7901fa1fcf8f24a7a646b5e0aa57bd8d3c0724ba
unknown <2612130020@ACADEMICO.IESB.BR>
2026-03-11 10:57:34 -0300

    adicionar codigos da aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,15 @@
+// inclusao de biblioteca da linguagem
+#include <stdio.h>
+
+int main() {
+    //1. Entrada
+
+    //2.Processamento
+
+    //3. Saida
+    printf("Amiguinhos do computador\n");
+    printf("eu amo minha namorada\n");
+    printf("%i\n", 10*47);
+
+    return 0;
+}
