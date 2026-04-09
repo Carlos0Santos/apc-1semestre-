6625fa86137704d098a1a1d433a91d580410e404
Carlos0Santos <55carlos0daniel55@gmail.com>
2026-04-09 17:48:22 -0300

    Atualiza declara_constantes.c

---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+
+    int primeiro = 10;
+    int segundo = -5;
+    int terceiro = 4;
+
+<<<<<<< HEAD
+    printf("%i %i %.5i", primeiro, segundo, terceiro);
+=======
+    printf("%i\n%i\n%.5i", primeiro, segundo, terceiro);
+>>>>>>> daef481b7edd4374535a1b5941549928d095ee7e
+
+    return 0;
+}
