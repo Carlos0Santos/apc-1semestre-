12a4f784d853a604a5a3969ba5cc47e12f99461d
Carlos0Santos <55carlos0daniel55@gmail.com>
2026-04-09 18:12:31 -0300

    fix: removendo marcas de conflito na pratica02

--- praticas/pratica02/imprima_formatado.c
+++ praticas/pratica02/imprima_formatado.c
@@ -25,7 +25,7 @@ Total                   229.85
     valor[0] = 39.99;
     valor[1] = 89.90;
     valor[2] = 19.99;
-<<<<<<< HEAD
+
     valor[3] = valor[0] + valor[1] + valor[2];
 
     printf("======================\n");
@@ -37,7 +37,7 @@ Total                   229.85
     printf("%s       %.3i      %.2f\n", produto[2],quantidade[2],valor[2]);
     printf("======================\n");
     printf("total              %.2f\n",valor[3]);
-=======
+
 
     float total;
     total = quantidade[0] * valor[0] +
@@ -53,7 +53,7 @@ Total                   229.85
     printf("%-10s       %03d      %10.2f\n", produto[2],quantidade[2],valor[2]);
     printf("=============================\n");
     printf("total              %10.2f\n",total);
->>>>>>> daef481b7edd4374535a1b5941549928d095ee7e
+
 
     return 0;
 }
