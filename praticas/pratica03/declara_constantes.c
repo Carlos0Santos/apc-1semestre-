f76c6ea2a6d633d1e90a73e1b6a7032c8ef8523b
Carlos0Santos <55carlos0daniel55@gmail.com>
2026-04-09 17:51:03 -0300

    Atualiza declara_constantes.c

---
+++ praticas/pratica03/declara_constantes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+
+
+    #define PI 3.14159265
+    const float EULER = 2.71828182;
+
+    printf ("Valor de PI: %.8f\n", PI);
+    printf ("Valor de Euler: %.8f\n", EULER);
+
+    return 0;
+}
