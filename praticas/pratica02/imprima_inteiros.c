f5b484b3d20b160ef1b7ab12ba629dc8225bdde6
Carlos0Santos <55carlos0daniel55@gmail.com>
2026-04-09 18:12:49 -0300

    fix: removendo marcas de conflito na pratica02

--- praticas/pratica02/imprima_inteiros.c
+++ praticas/pratica02/imprima_inteiros.c
@@ -6,11 +6,10 @@ int main () {
     int segundo = -5;
     int terceiro = 4;
 
-<<<<<<< HEAD
     printf("%i %i %.5i", primeiro, segundo, terceiro);
-=======
+
     printf("%i\n%i\n%.5i", primeiro, segundo, terceiro);
->>>>>>> daef481b7edd4374535a1b5941549928d095ee7e
+
 
     return 0;
 }
