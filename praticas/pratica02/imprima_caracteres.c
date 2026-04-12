bfe67706f313cd943d7318e1f40ada6c75e73a08
Carlos0Santos <55carlos0daniel55@gmail.com>
2026-04-12 00:50:58 -0300

    feat: conclui pratica04. Fecha #7

--- praticas/pratica02/imprima_caracteres.c
+++ praticas/pratica02/imprima_caracteres.c
@@ -2,17 +2,10 @@
 
 int main () {
 
-<<<<<<< HEAD
     char primeiro = 'a';
     char segundo[] = "Isso aqui eh uma string";
 
-    printf("%c \n %s",primeiro,segundo);
-=======
-    char primeiro = 'A';
-    char segundo[] = "Isso aqui eh uma string";
-
-    printf("%c\n%s",primeiro,segundo);
->>>>>>> daef481b7edd4374535a1b5941549928d095ee7e
+    printf("%c \n %s",primeiro,segundo);   
 
     return 0;
 }
--- praticas/pratica02/imprima_formatado.c
+++ praticas/pratica02/imprima_formatado.c
@@ -25,20 +25,8 @@ Total                   229.85
     valor[0] = 39.99;
     valor[1] = 89.90;
     valor[2] = 19.99;
-<<<<<<< HEAD
-    valor[3] = valor[0] + valor[1] + valor[2];
 
-    printf("======================\n");
-    printf("      NOTA LEGAl\n");
-    printf("======================\n");
-    printf("Produto     Qtd Valor Unit\n");
-    printf("%s       %.3i      %.2f\n", produto[0],quantidade[0],valor[0]);
-    printf("%s       %.3i      %.2f\n", produto[1],quantidade[1],valor[1]);
-    printf("%s       %.3i      %.2f\n", produto[2],quantidade[2],valor[2]);
-    printf("======================\n");
-    printf("total              %.2f\n",valor[3]);
-=======
-
+  
     float total;
     total = quantidade[0] * valor[0] +
             quantidade[1] * valor[1] +
@@ -53,7 +41,6 @@ Total                   229.85
     printf("%-10s       %03d      %10.2f\n", produto[2],quantidade[2],valor[2]);
     printf("=============================\n");
     printf("total              %10.2f\n",total);
->>>>>>> daef481b7edd4374535a1b5941549928d095ee7e
 
     return 0;
 }
