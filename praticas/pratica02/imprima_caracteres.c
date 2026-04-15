9fe2a8ad725297b79ae121a4037d6f43adb98159
Carlos0Santos <55carlos0daniel55@gmail.com>
2026-04-15 10:44:24 -0300

    corrigindo conflito

--- praticas/pratica02/imprima_caracteres.c
+++ praticas/pratica02/imprima_caracteres.c
@@ -5,11 +5,7 @@ int main () {
     char primeiro = 'a';
     char segundo[] = "Isso aqui eh uma string";
 
-<<<<<<< HEAD
-    printf("%c \n %s",primeiro,segundo);   
-=======
     printf("%c \n %s",primeiro,segundo);
->>>>>>> f74fe6e1ee917d9cdaa0b4c9a380d676d2cbe543
 
     return 0;
 }
